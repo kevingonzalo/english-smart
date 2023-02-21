de1d06d962f5ce532d03b4b69b70f4de6b973b67
kevin balmaceda <kevinbalmaceda5@gmail.com>
2023-02-20 21:08:37 -0300

    english smart v1.1 primer commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,11 +7,19 @@ import { useState } from "react";
 export default function Navbar() {
   let [sesion, setOpenSesion] = useState(false);
   let [register, setRegister] = useState(false);
+  const [clicked, setClicked] = useState(false);
+  // muestra boton menu hamburguesa cuando baje el ancho de la pantalla
+  const activeMenu = () => {
+    setClicked(!clicked);
+  };
+  // para abrir formulario de inicio de sesion y registro
   const openSesion = () => {
+    activeMenu();
     setRegister((register = false));
     setOpenSesion(!sesion);
   };
   const openRegister = () => {
+    activeMenu();
     setOpenSesion((sesion = false));
     setRegister(!register);
   };
@@ -21,27 +29,32 @@ export default function Navbar() {
   };
   return (
     <section className="navbar">
+      <button onClick={activeMenu} className={`btn-menu active ${clicked ? "btn-menu-activado" : ""}`}>
+        <i className="fa-solid fa-bars"></i>
+      </button>
       <a href="/">
         <img src={Logo} className="logo" alt="Logo de la Página web English Smart" />
       </a>
-      <ul class="nav justify-content-center">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/">
-            Home
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#cursos">
-            Cursos
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#contacto">
-            Contacto
-          </a>
-        </li>
-      </ul>
-      <div className="btn-container">
+      <div className={`nav-lista ${clicked ? "active" : ""}`.trim()}>
+        <ul className={`nav justify-content-center ${clicked ? "active" : ""}`.trim()}>
+          <li className="nav-item">
+            <a className="nav-link active" onClick={activeMenu} aria-current="page" href="/">
+              Home
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" onClick={activeMenu} href="#cursos">
+              Cursos
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" onClick={activeMenu} href="#contacto">
+              Contacto
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div className={`btn-container  ${clicked ? "active" : ""}`.trim()}>
         <button onClick={openSesion}>Iniciar Sesión</button>
         <button onClick={openRegister}>Registrarse</button>
       </div>
