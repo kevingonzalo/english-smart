1728ca49714df4a079922d7c85f413029ab9f4cb
kevin balmaceda <kevinbalmaceda5@gmail.com>
2023-02-28 16:47:54 -0300

    arregle y ordene las carpetas styles

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import "./components/variables.css";
+import "./components/styles/variables.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
